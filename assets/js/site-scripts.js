9f3b18c6631f15e2f11a635549447ef482cd9089
shinTM <emasugeta@gmail.com>
2017-10-14 15:29:18 +0300

    upd: main styles

--- assets/js/site-scripts.js
+++ assets/js/site-scripts.js
@@ -1,5 +1,5 @@
 (function($){
-	
+
 	$( document ).ready( function() {
 		new WOW().init();
 
@@ -8,22 +8,44 @@
 				$parent = $this.parent( '.main-menu' );
 
 			$parent.toggleClass( 'menu_state_open' );
-   	 	});
-   	 	
-   	 	$('div[data-type="background"]').each(function(){
-    	var $bgobj = $(this);
+		});
 
- 		$( window ).scroll( function() {
-        	var yPos = -( $( window ).scrollTop() / $bgobj.data('speed')); 
-        	var coords = '50% '+ yPos + 'px';
-     
-        	$bgobj.css({ backgroundPosition: coords });
-    	} ); 
+		$('div[data-type="background"]').each(function(){
+			var $bgobj = $(this);
+
+			$( window ).scroll( function() {
+				var yPos = -( $( window ).scrollTop() / $bgobj.data('speed'));
+				var coords = '50% '+ yPos + 'px';
+
+				$bgobj.css({ backgroundPosition: coords });
+			} );
+
+		});
+
+		var countdown = $('.countdown');
+
+		countdown.downCount( {
+			date: '10/16/2017 14:19:00',
+			offset: +3
+		}, function () {
+		} );
+
+		var curVal;
+
+		$('.activities .likes').on( 'click', function(){
+
+			var $this = $( this ),
+				$val  = $( '.value', $this ),
+				initVal = +$this.data('init');
+
+				$val.html(initVal++);
+
+				$this.data('init', initVal);
+
+			return false;
+
+		} )
 
 	});// end ready
-	})
 
-
-})
-
-(jQuery);
+})( jQuery );
